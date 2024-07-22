219006d634ad36a4d07fa628649131ea40fd4d21
balu217-git <vangapandubalaraju.git@gmail.com>
2024-07-22 09:18:32 +0530

    sss

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,8 +27,32 @@
 
   } )();
 
+  $(window).resize(function() {
+    gridAdjust(".info-features-mobile-slider .feature");
+    gridAdjust(".info-benefits-mobile-slider .benefit");
+    gridAdjust(".info-features-mobile-slider .info-card");
+  
+  });
+  
+  $(window).on('load', function() {
+    gridAdjust(".info-features-mobile-slider .feature");
+    gridAdjust(".info-benefits-mobile-slider .benefit");
+    gridAdjust(".info-testimonials-mobile-slider .info-card");
+  
+  
+  //     if ($(window).width() < 767) {
+  //       gridAdjust(".features-card .info-list-item");
+  //    }
+    });
+
 
   $(document).ready(function(){
+
+    mobileSlider(".info-features-mobile-slider", 1, true, false, 767);
+    mobileSlider(".info-benefits-mobile-slider", 1, true, false, 767);
+    mobileSlider(".info-testimonials-mobile-slider", 1, true, false, 767);
+    
+
     const messages = [
         "REAL-TIME",
         "HASSLE- FREE"
@@ -41,4 +65,64 @@
             $('#slide-text').text(messages[currentIndex]).slideDown(1000);
         });
     }, 2000); // Change text every 4 seconds
+
 });
+
+var mobileSlider = ($slidername, $slidesToShow, $dots, $arrows, $breakpoint) => {
+
+    var slider = $($slidername);
+      var settings = {
+        mobileFirst: true,
+        slidesToShow: $slidesToShow,
+        slidesToScroll: 1,
+        dots: $dots,
+        arrows: $arrows,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        infinite: false,
+        responsive: [
+          {
+            breakpoint: $breakpoint,
+            settings: "unslick"
+          }
+        ]
+      };
+  
+      slider.slick(settings);
+      // slider.on('laod', function(event, slick, currentSlide, nextSlide){
+      //     let actives = slider.find(".slick-active").addClass('balaraju');
+      // });
+      slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+          slider.find(".slick-slide").removeClass('works');
+          slider.find('.slick-current').addClass('works');
+          slider.find('.slick-current').toggleClass("hovered");
+       });
+  
+      $(window).on("resize", function () {
+        if ($(window).width() > $breakpoint) {
+  
+          return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+          return slider.slick(settings);
+        }
+      });
+  }
+
+  function gridAdjust(targertSteing) {
+	// if ($(window).width() < 767) {
+	var targertHight = $(targertSteing);
+	  targertHight.css('height', 'auto');
+	   var heights = [];
+	   targertHight.each(function() {
+		 var elem = $(this);
+		 var height = elem.outerHeight();
+		 heights.push(height);
+	   });
+	   heights = heights.sort(function(a, b) {
+		 return b - a
+	   });
+	   var tallest = heights[0];
+	   targertHight.css('height', tallest + 'px');
+	// }
+  }
